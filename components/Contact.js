f6bc5b9f2ff956664c530dc631e375b09d59dc9b
Peter Hilden <peter.e.hilden@gmail.com>
2020-11-16 15:02:42 +0200

    initialized contact form

--- components/Contact.js
+++ components/Contact.js
@@ -1,7 +1,45 @@
+import { useState } from 'react'
 import styles from '../styles/Contact.module.css'
 
-const Contact = () =>  {
-    return <div>Contact</div>
+const Contact = () => {
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  const submit = async () => {
+    
+    if (!name || !email || !message) {
+      console.log('Missing info')
+      return
+    }
+    
+    const body = JSON.stringify({
+      name,
+      phone,
+      email,
+      message
+    })
+    await fetch('/api/email', { method: 'POST', body: body })
   }
-  
-  export default Contact
+
+
+  return <div className={styles.container}>
+    <div className={styles.label}>OTA YHTEYTTÄ</div>
+    <div className={styles.inputWrapper}>
+      <input className={styles.textInput} placeholder='Nimi*' value={name} onChange={event => setName(event.target.value)}></input>
+    </div>
+    <div className={styles.inputWrapper}>
+      <input className={styles.textInput} placeholder='Puhelinnumero' value={phone} onChange={event => setPhone(event.target.value)}></input>
+    </div>
+    <div className={styles.inputWrapper}>
+      <input className={styles.textInput} placeholder='Sähköposti*' value={email} onChange={event => setEmail(event.target.value)}></input>
+    </div>
+    <div className={styles.inputWrapper}>
+      <textarea rows={4} className={styles.textInput} placeholder='Viesti*' value={message} onChange={event => setMessage(event.target.value)}></textarea>
+    </div>
+    <div className={styles.submitButton} onClick={submit}>Lähetä</div>
+  </div>
+}
+
+export default Contact
---
+++ pages/api/email.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer')
+
+export default async (req, res) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  })
+
+  const { name, email, phone, message } = JSON.parse(req.body)
+
+  const content = {
+    to: process.env.EMAIL,
+    from: process.env.EMAIL,
+    replyTo: email,
+    subject: `Uusi viesti henkilöltä ${name}`,
+    text: message,
+    html: `
+    <p>Nimi: ${name}</p>
+    <p>Sähköposti: ${email}</p>
+    <p>Puhelinnumero: ${phone}</p>
+    <p>Viesti: ${message}</p>
+    `,
+  }
+
+  try {
+    await transporter.sendMail(content)
+    res.status(200).send('Message sent successfully.')
+  } catch (error) {
+    console.log('ERROR', error)
+    res.status(400).send('Message not sent.')
+  }
+}
