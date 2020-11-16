ef6cbc0a6450f0583b002efce3d7e9453913f920
baumis <baumgartneraxel@gmail.com>
2020-11-16 13:55:34 +0200

    slider

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
-import { faBars, faCocktail, faHamburger } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCocktail, faHamburger, faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 import styles from '../../styles/Navbar.module.css'
+import Slider from './Slider'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
 
     return (
         <div className={styles.container}>
@@ -14,9 +17,14 @@ const Navbar = () => {
                 <div className={styles.listItem}>Asiakaskokemukset</div>
                 <div className={styles.listItem}>Ota yhteyttä</div>
             </div>
-            <div className={styles.navigationButton}>
-                <FontAwesomeIcon icon={faBars} />
+            <div className={styles.navigationButton} onClick={() => setOpen(!open)}>
+                {open?
+                    <FontAwesomeIcon icon={faTimes} />
+                    :
+                    <FontAwesomeIcon icon={faBars} />
+                }
             </div>
+            <Slider open={open}/>
         </div>
     )
 }
