8982039d0060f1c16687a2f7f3578405f5f467bb
Peter Hildén <peter.e.hilden@gmail.com>
2020-11-20 14:21:37 +0200

    added styles to stories and added breakpoints to contact form

--- components/Stories/Stories.js
+++ components/Stories/Stories.js
@@ -1,11 +1,14 @@
 import styles from '../../styles/Stories.module.css'
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
+import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Stories = ({ stories }) => {
   return <div className={styles.container}>
     <div className={styles.label}>ASIAKASTARINAT</div>
     {stories.map(story =>
       <div className={styles.storyWrapper} key={story.name}>
+        <FontAwesomeIcon className={styles.quoteIcon} icon={faQuoteLeft}/>
         <div className={styles.story} dangerouslySetInnerHTML={{ __html: documentToHtmlString(story.story)}}></div>
         <div className={styles.name}>{story.name}</div>
       </div>
