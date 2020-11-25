d20d3cab68980b4c8034cdaeb4fd7905090a781b
Baumis <baumgartneraxel@gmail.com>
2020-11-25 19:26:57 +0200

    about section

--- components/About/About.js
+++ components/About/About.js
@@ -2,18 +2,15 @@ import styles from '../../styles/About.module.css'
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 
 const About = ({ about }) => {
-  console.log(about)
   return (
     <div className={styles.container}>
-      <div className={styles.picture}>
-        <div className={styles.image} style={{ backgroundImage: `url(https://images.unsplash.com/photo-1560448204-603b3fc33ddc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)` }} >
+      <div className={styles.title}>{about.fields.title}</div>
+      <div className={styles.content}>
+        <div className={styles.picture} style={{ backgroundImage: `url(https://images.unsplash.com/photo-1560448204-603b3fc33ddc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)` }}>
         </div>
-        <div className={styles.imageBorder}>
+        <div className={styles.presentation}>
+          <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(about.fields.presentation) }} className={styles.text}></div>
         </div>
-      </div>
-      <div className={styles.presentation}>
-        <div>{about.fields.title}</div>
-        <div dangerouslySetInnerHTML={{ __html: documentToHtmlString(about.fields.presentation) }}></div>
       </div>
     </div>
   )
