65b9cdc22a4d6862718545d7110ca4f5b1613096
Baumis <baumgartneraxel@gmail.com>
2020-11-25 18:56:11 +0200

    nre picture

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,13 +4,12 @@ import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 const Header = ({ header }) => {
   
   return (
-    //<div className={styles.container} style={{backgroundImage: `url(${header.picture})`}}>
-    <div className={styles.container} style={{ backgroundImage: `url(https://images.unsplash.com/photo-1550581190-9c1c48d21d6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1898&q=80)` }}>
-      <div className={styles.mikosSpace}>
-      </div>
+    <div className={styles.container} style={{backgroundImage: `url(${header.picture})`}}>
       <div className={styles.information}>
         <div className={styles.title}>{header.fields.name}</div>
         <div className={styles.text} dangerouslySetInnerHTML={{ __html: documentToHtmlString(header.fields.description) }}></div>
+      </div>
+      <div className={styles.mikosSpace}>
       </div>
     </div>
   )
