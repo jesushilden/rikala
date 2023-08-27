350d79124d589c06b43e43b276ecf58570640240
Hildén Peter <ext-peter.1.hilden@sok.fi>
2023-08-27 10:20:37 +0300

    remove animation mobile

--- components/Process/TimelineMobile.js
+++ components/Process/TimelineMobile.js
@@ -1,39 +1,9 @@
 import styles from '../../styles/TimelineMobile.module.css'
-import { faChartBar, faPhotoVideo, faSignature, faTrophy, faUsers, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faChartBar, faPhotoVideo, faUsers, faHome } from '@fortawesome/free-solid-svg-icons'
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { useEffect } from 'react'
 
 const TimelineMobile = ({ process }) => {
-
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, [])
-
-  function onScroll() {
-    const nodePosition1 = document.getElementById("node1").offsetTop;
-    const nodePosition2 = document.getElementById("node2").offsetTop;
-    const nodePosition3 = document.getElementById("node3").offsetTop;
-    const nodePosition4 = document.getElementById("node4").offsetTop;
-    const nodePosition5 = document.getElementById("node5").offsetTop;
-
-    if (window.pageYOffset + window.innerHeight > nodePosition1 + 200) {
-      document.getElementById("node1").style.opacity = '1'
-    }
-    if (window.pageYOffset + window.innerHeight > nodePosition2 + 200) {
-      document.getElementById("node2").style.opacity = '1'
-    }
-    if (window.pageYOffset + window.innerHeight > nodePosition3 + 200) {
-      document.getElementById("node3").style.opacity = '1'
-    }
-    if (window.pageYOffset + window.innerHeight > nodePosition4 + 200) {
-      document.getElementById("node4").style.opacity = '1'
-    }
-    if (window.pageYOffset + window.innerHeight > nodePosition5 + 200) {
-      document.getElementById("node5").style.opacity = '1'
-    }
-  }
 
   const getStepByOrder = (order) => {
     return process.find(item => item.order === order)
@@ -49,7 +19,7 @@ const TimelineMobile = ({ process }) => {
           <div className={styles.line}></div>
           <div className={styles.circle}><FontAwesomeIcon icon={faPhotoVideo} /></div>
         </div>
-        <div className={`${styles.rightColumn} ${styles.textColumn}`} id="node1">
+        <div className={styles.rightColumn} id="node1">
           <div className={styles.title}>{getStepByOrder(1).name}</div>
           <div className={styles.info} dangerouslySetInnerHTML={{ __html: documentToHtmlString(getStepByOrder(1).description) }}></div>
         </div>
@@ -59,7 +29,7 @@ const TimelineMobile = ({ process }) => {
           <div className={styles.line}></div>
           <div className={styles.circle}><FontAwesomeIcon icon={faChartBar} /></div>
         </div>
-        <div className={`${styles.rightColumn} ${styles.textColumn}`} id="node2">
+        <div className={styles.rightColumn} id="node2">
           <div className={styles.title}>{getStepByOrder(2).name}</div>
           <div className={styles.info} dangerouslySetInnerHTML={{ __html: documentToHtmlString(getStepByOrder(2).description) }}></div>
         </div>
@@ -69,7 +39,7 @@ const TimelineMobile = ({ process }) => {
           <div className={styles.line}></div>
           <div className={styles.circle}><FontAwesomeIcon icon={faUsers} /></div>
         </div>
-        <div className={`${styles.rightColumn} ${styles.textColumn}`} id="node3">
+        <div className={styles.rightColumn} id="node3">
           <div className={styles.title}>{getStepByOrder(3).name}</div>
           <div className={styles.info} dangerouslySetInnerHTML={{ __html: documentToHtmlString(getStepByOrder(3).description) }}></div>
         </div>
@@ -79,7 +49,7 @@ const TimelineMobile = ({ process }) => {
           <div className={styles.line}></div>
           <div className={styles.circle}><FontAwesomeIcon icon={faUsers} /></div>
         </div>
-        <div className={`${styles.rightColumn} ${styles.textColumn}`} id="node4">
+        <div className={styles.rightColumn} id="node4">
           <div className={styles.title}>{getStepByOrder(4).name}</div>
           <div className={styles.info} dangerouslySetInnerHTML={{ __html: documentToHtmlString(getStepByOrder(4).description) }}></div>
         </div>
@@ -93,7 +63,7 @@ const TimelineMobile = ({ process }) => {
           <div className={styles.centerEndEmpty}>
           </div>
         </div>
-        <div className={`${styles.rightEndColumn} ${styles.textColumn}`} id="node5">
+        <div className={styles.rightEndColumn} id="node5">
         <div className={styles.title}>{getStepByOrder(5).name}</div>
           <div className={styles.info} dangerouslySetInnerHTML={{ __html: documentToHtmlString(getStepByOrder(5).description) }}></div>
         </div>
