105b0cac42ac617f2f65f1525d184c5e6bc10604
Hildén Peter <ext-peter.1.hilden@sok.fi>
2023-11-05 10:31:08 +0200

    fix scripts

--- pages/index.js
+++ pages/index.js
@@ -26,20 +26,31 @@ export default function Home(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta name="google-site-verification" content="Avw38jTiFG68jb_viHt2IkvImJOEOXXI9thTvKv_ekY" />
         <link rel="icon" href="/MR_white.ico" />
-        <Script key="function" id="facebook-pixel-script">
-          {`!function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1012482046636079');
-          fbq('track', 'PageView');`}
-        </Script>
-        <noscript key="image" id="facebook-pixel-image">
-            <img alt="facebook pixel image" height="1" width="1" style={{display: "none"}} src="https://www.facebook.com/tr?id=1012482046636079&ev=PageView&noscript=1"/>
+        <Script
+            id="facebook-pixel-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1012482046636079');
+                fbq('track', 'PageView');
+              `,
+            }}
+        />
+        <noscript id="facebook-pixel-image">
+          <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1012482046636079&ev=PageView&noscript=1"
+          />
         </noscript>
       </Head>
 
